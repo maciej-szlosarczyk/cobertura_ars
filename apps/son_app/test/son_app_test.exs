dd14eddd4c06cc7abf9e3c8c0992c1e95994f970
Maciej Szlosarczyk <maciej@sosek.net>
2024-08-31 09:24:27 +0300

    Test one

--- apps/son_app/test/son_app_test.exs
+++ apps/son_app/test/son_app_test.exs
@@ -5,4 +5,8 @@ defmodule SonAppTest do
   test "greets the world" do
     assert SonApp.hello() == :world
   end
+
+  test "one arm is tested" do
+    assert SonApp.not_tested(1) == :tested
+  end
 end
